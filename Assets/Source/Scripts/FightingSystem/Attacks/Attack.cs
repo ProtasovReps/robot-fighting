df99997d34269d79c3c7796aeb1df17150b6b56b
protasov <kostyapk3@gmail.com>
2025-09-11 17:23:03 +0300

    Add base leg super attack

--- Assets/Source/Scripts/FightingSystem/Attacks/Attack.cs
+++ Assets/Source/Scripts/FightingSystem/Attacks/Attack.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace FightingSystem.Attacks
 {
@@ -26,6 +27,7 @@ namespace FightingSystem.Attacks
         public async UniTask Launch(CancellationTokenSource tokenSource)
         {
             await UniTask.WaitForSeconds(_startDelay, cancellationToken: tokenSource.Token, cancelImmediately: true);
+            Debug.LogError("biu");
             Execute(_damage);
             await UniTask.WaitForSeconds(_endDelay, cancellationToken: tokenSource.Token, cancelImmediately: true);
         }
