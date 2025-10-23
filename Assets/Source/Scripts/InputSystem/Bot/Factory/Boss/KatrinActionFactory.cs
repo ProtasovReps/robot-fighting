1f913bcc6afd6990da3448ec583417b059e7d935
protasov <kostyapk3@gmail.com>
2025-10-23 16:17:06 +0300

    Add Katrin boss

---
+++ Assets/Source/Scripts/InputSystem/Bot/Factory/Boss/KatrinActionFactory.cs
@@ -0,0 +1,21 @@
+﻿using Extensions;
+using FiniteStateMachine;
+using FiniteStateMachine.States;
+using InputSystem.Bot.Executor;
+
+namespace InputSystem.Bot.Factory.Boss
+{
+    public class KatrinActionFactory : ActionFactory
+    {
+        protected override void AddActions(ActionStash stash, BotInputStateMachine machine)
+        {
+            BotAction upAttack = stash.Get(MotionHashes.ArmAttack);
+            BotAction super = stash.Get(MotionHashes.Super);
+            BotAction downAttack = stash.Get(MotionHashes.LegAttack);
+            BotAction block = stash.Get(MotionHashes.Block);
+            
+            new RandomActionExecutor<PlayerNearbyState>(machine, upAttack, downAttack, block);
+            new RandomActionExecutor<ValidAttackDistanceState>(machine, super);
+        }
+    }
+}
