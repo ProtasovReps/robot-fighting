ce1a676a264b085a646dd7ad8d0b0f66aba5208f
protasov <kostyapk3@gmail.com>
2025-10-26 18:06:12 +0300

    Setup bot parameters

--- Assets/Source/Scripts/CharacterSystem/Data/BotParameters.cs
+++ Assets/Source/Scripts/CharacterSystem/Data/BotParameters.cs
@@ -7,9 +7,9 @@ namespace CharacterSystem.Parameters
     {
         [field: SerializeField, Min(0.1f)] public float MoveDuration { get; private set; }
         [field: SerializeField, Min(0.1f)] public float IdleDuration { get; private set; }
-        [field: SerializeField, Min(1f)] public float BaseDamage { get; private set; }
-        [field: SerializeField, Min(1f)] public float BaseImpulse { get; private set; }
-        [field: SerializeField, Min(1f)] public float AttackInputDelay { get; private set; }
+        [field: SerializeField] public float BaseDamage { get; private set; }
+        [field: SerializeField] public float AttackInputDelay { get; private set; }
+        [field: SerializeField, Min(0f)] public float BaseImpulse { get; private set; }
         [field: SerializeField] public DamageType DamageType { get; private set; }
     }
 }
