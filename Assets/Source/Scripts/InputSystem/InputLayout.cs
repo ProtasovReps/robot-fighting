d2f485104e6a3451d26ee81a710135dffe9ece84
protasov <kostyapk3@gmail.com>
2025-11-26 20:35:17 +0300

    Add pc and mobile input layouts

---
+++ Assets/Source/Scripts/InputSystem/InputLayout.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using YG;
+
+namespace InputSystem
+{
+    public class InputLayout : MonoBehaviour
+    {
+        [SerializeField] private Transform[] _mobileLayout;
+        [SerializeField] private Transform _pCLayout;
+
+        private void Awake()
+        {
+            if (YG2.envir.isDesktop)
+            {
+                _pCLayout.gameObject.SetActive(true);
+            }
+            else
+            {
+                for (int i = 0; i < _mobileLayout.Length; i++)
+                {
+                    _mobileLayout[i].gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
