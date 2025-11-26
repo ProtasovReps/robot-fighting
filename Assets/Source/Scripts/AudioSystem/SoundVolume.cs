3ff7bf8ceb94a79636ebe9fe841a4fea3da206a3
protasov <kostyapk3@gmail.com>
2025-11-26 15:38:57 +0300

    Add SoundVolume

---
+++ Assets/Source/Scripts/AudioSystem/SoundVolume.cs
@@ -0,0 +1,45 @@
+﻿using Ami.BroAudio;
+using UnityEngine;
+using UnityEngine.UI;
+using YG;
+
+namespace AudioSystem
+{
+    public class SoundVolume : MonoBehaviour
+    {
+        private const float MaxSound = 0.05f;
+        
+        [SerializeField] private Slider _slider;
+
+        private void Awake()
+        {
+            _slider.maxValue = MaxSound;
+            _slider.value = YG2.saves.SoundVolume;
+            
+            BroAudio.SetVolume(_slider.value);
+        }
+
+        private void OnEnable()
+        {
+            _slider.onValueChanged.AddListener(OnSliderValueChanged);
+        }
+
+        private void OnDisable()
+        {
+            _slider.onValueChanged.RemoveListener(OnSliderValueChanged);
+        }
+
+        private void OnDestroy()
+        {
+            YG2.saves.SoundVolume = _slider.value;
+            YG2.SaveProgress();
+        }
+
+        private void OnSliderValueChanged(float newValue)
+        {
+            _slider.value = newValue;
+
+            BroAudio.SetVolume(newValue);
+        }
+    }
+}
--- Assets/Source/Scripts/Yandex/SavesYG.cs
+++ Assets/Source/Scripts/Yandex/SavesYG.cs
@@ -9,7 +9,8 @@ namespace YG
         public float SpeedStat;
         public float DamageStat;
         public float BlockStat;
-        public int SceneIndex = 3; 
+        public int SceneIndex = 3;
+        public float SoundVolume = 0.035f;
         public bool IsGuidePassed = false;
         public List<string> Implants = new();
         public List<string> Fighters = new();
