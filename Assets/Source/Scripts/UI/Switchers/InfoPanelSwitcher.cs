0eefe75f4278b907aa056d97d7348e83043317b9
protasov <kostyapk3@gmail.com>
2025-11-27 23:10:19 +0300

    Fix minor

--- Assets/Source/Scripts/UI/Switchers/InfoPanelSwitcher.cs
+++ Assets/Source/Scripts/UI/Switchers/InfoPanelSwitcher.cs
@@ -1,15 +1,18 @@
 ﻿using Cysharp.Threading.Tasks;
 using UI.SliderView;
 using UnityEngine;
+using YG;
 
 namespace UI.Effect
 {
     public class InfoPanelSwitcher : AnimatablePanelSwitcher
     {
+        [SerializeField] private LeaderboardYG _leaderboard;
         [SerializeField] private ScrollbarScroller _scrollbarScroller;
         
         protected override UniTaskVoid Switch()
         {
+            _leaderboard.gameObject.SetActive(true);
             _scrollbarScroller.Scroll().Forget();
             return base.Switch();
         }
