9dd179520d62c7dbd4c85659aa57f2e06b39ef56
protasov <kostyapk3@gmail.com>
2025-11-27 21:10:48 +0300

    Fix leaderboard

---
+++ Assets/Source/Scripts/Yandex/SaveResetButton.cs
@@ -0,0 +1,24 @@
+﻿using R3;
+using UI.Buttons;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace YG
+{
+    public class SaveResetButton : MonoBehaviour
+    {
+        [SerializeField] private UnitButton _unitButton;
+
+        private void Awake()
+        {
+            _unitButton.Pressed.Subscribe(_ => EgorLetov()).AddTo(this);
+        }
+
+        private void EgorLetov()
+        {
+            YG2.SetDefaultSaves();
+            YG2.SaveProgress();
+            SceneManager.LoadScene(3);
+        }
+    }
+}
