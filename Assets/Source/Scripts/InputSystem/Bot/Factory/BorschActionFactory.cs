2ad2015293e13eb302dec417ef3761e4e63097e8
protasov <kostyapk3@gmail.com>
2025-11-28 16:41:02 +0300

    Add guide action executors

---
+++ Assets/Source/Scripts/InputSystem/Bot/Factory/BorschActionFactory.cs
@@ -0,0 +1,17 @@
+﻿using Extensions;
+using FiniteStateMachine;
+using FiniteStateMachine.States;
+using InputSystem.Bot.Executor;
+
+namespace InputSystem.Bot.Factory
+{
+    public class BorschActionFactory : ActionFactory
+    {
+        protected override void AddActions(ActionStash stash, BotInputStateMachine machine, Disposer disposer)
+        {
+            BotAction upAttack = stash.Get(MotionHashes.ArmAttack);
+            
+            disposer.Add(new SoloActionExecutor<ValidAttackDistanceState>(machine, upAttack));
+        }
+    }
+}
---
+++ Assets/Source/Scripts/InputSystem/Bot/Factory/JerryActionFactory.cs
@@ -0,0 +1,12 @@
+﻿using Extensions;
+using FiniteStateMachine;
+
+namespace InputSystem.Bot.Factory
+{
+    public class JerryActionFactory : ActionFactory
+    {
+        protected override void AddActions(ActionStash stash, BotInputStateMachine machine, Disposer disposer)
+        {
+        }
+    }
+}
