6284164d005af735d6be3b7a08ab9f64f7182e76
protasov <kostyapk3@gmail.com>
2025-11-28 12:42:07 +0300

    Add review shower

---
+++ Assets/Source/Scripts/Yandex/ReviewShower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FiniteStateMachine;
+using FiniteStateMachine.States;
+using R3;
+using Reflex.Attributes;
+using UnityEngine;
+
+namespace YG
+{
+    public class ReviewShower : MonoBehaviour
+    {
+        [SerializeField] private float _delay;
+        
+        [Inject]
+        private void Inject(BotStateMachine machine)
+        {
+            machine.Value
+                .Delay(TimeSpan.FromSeconds(_delay))
+                .Where(state => state is DeathState)
+                .Subscribe(_ => YG2.ReviewShow())
+                .AddTo(this);
+        }
+    }
+}
